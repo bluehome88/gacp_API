88fe0b71934bc1885755db08755dcbf8fa2add48
Blue Webmaster <bluehome88@outlook.com>
2021-11-17 09:11:14 -0800

    MemberClick Config

---
+++ config.php
@@ -0,0 +1,8 @@
+<?php
+	const orgId = 'gacp';
+	const BASE_URL = "https://".orgId.".memberclicks.net";
+	const clientId = 'IZyoWzdUOV57ZPbg2MiB';
+	const clientSecret = "e66c4cfda95d496aa811326f4dee15d2";
+	const redirectURI = "https://api.map-dynamics.com";
+	const clientCredentials = 'Basic SVp5b1d6ZFVPVjU3WlBiZzJNaUI6ZTY2YzRjZmRhOTVkNDk2YWE4MTEzMjZmNGRlZTE1ZDI=';
+?>
